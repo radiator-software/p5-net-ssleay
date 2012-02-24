cd72eacfe7c7e89feff6f63c71d65f538ae1357f
Mike McCauley <mikem@airspayce.com>
2012-02-24 22:52:55 +0000

    Added mising doc for SESSION_cmp. Patch by paul.
    git-svn-id: r306

--- lib/Net/SSLeay.pm
+++ lib/Net/SSLeay.pm
@@ -61,7 +61,7 @@ $Net::SSLeay::slowly = 0;
 $Net::SSLeay::random_device = '/dev/urandom';
 $Net::SSLeay::how_random = 512;
 
-$VERSION = '1.44';
+$VERSION = '1.45';
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(
     AT_MD5_WITH_RSA_ENCRYPTION
