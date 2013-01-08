8a70d3fd46adc73f2688889def954ce6256a23a0
Mike McCauley <mikem@airspayce.com>
2013-01-08 23:11:08 +0000

    Rebuild package with gnu format tar, to prevent problems with unpacking      on other systems such as old Solaris,
    git-svn-id: r365

--- lib/Net/SSLeay.pm
+++ lib/Net/SSLeay.pm
@@ -61,7 +61,7 @@ $Net::SSLeay::slowly = 0;
 $Net::SSLeay::random_device = '/dev/urandom';
 $Net::SSLeay::how_random = 512;
 
-$VERSION = '1.51';
+$VERSION = '1.52';
 @ISA = qw(Exporter);
 
 #BEWARE:
