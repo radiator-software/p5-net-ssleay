86525e453adba2d005e89fbe2897f53df68cf383
Mike McCauley <mikem@airspayce.com>
2018-01-16 04:43:47 +0000

    Version 1.83
    git-svn-id: r514

--- lib/Net/SSLeay.pm
+++ lib/Net/SSLeay.pm
@@ -63,7 +63,7 @@ $Net::SSLeay::slowly = 0;
 $Net::SSLeay::random_device = '/dev/urandom';
 $Net::SSLeay::how_random = 512;
 
-$VERSION = '1.82'; # Dont forget to set version in META.yml too
+$VERSION = '1.83'; # Dont forget to set version in META.yml too
 @ISA = qw(Exporter);
 
 #BEWARE:
