d7e23cc5671cfe88f68cde55195038b78a167ef9
Chris Novakovic <chris@chrisn.me.uk>
2019-05-08 15:07:10 +0100

    Developer release 1.86_11
    Bump version number in SSLeay.pm and Handle.pm from 1.86_10 to 1.86_11,
    and associate all reported changes since then with version 1.86_11.
    
    Closes #142.

--- lib/Net/SSLeay.pm
+++ lib/Net/SSLeay.pm
@@ -66,7 +66,7 @@ $Net::SSLeay::slowly = 0;
 $Net::SSLeay::random_device = '/dev/urandom';
 $Net::SSLeay::how_random = 512;
 
-$VERSION = '1.86_10'; # Also update $Net::SSLeay::Handle::VERSION
+$VERSION = '1.86_11'; # Also update $Net::SSLeay::Handle::VERSION
 @ISA = qw(Exporter);
 
 #BEWARE:
