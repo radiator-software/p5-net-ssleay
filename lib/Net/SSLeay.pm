d284937d22db64b62dd301014463728c569cb52f
Chris Novakovic <chris@chrisn.me.uk>
2021-12-29 19:55:04 +0000

    Developer release 1.91_02
    Bump all version numbers from 1.90_01 to 1.91_02, and associate all
    reported changes since 1.90_01 with version 1.91_02.
    
    Closes #348.

--- lib/Net/SSLeay.pm
+++ lib/Net/SSLeay.pm
@@ -70,7 +70,7 @@ $Net::SSLeay::how_random = 512;
 #   inc/Test/Net/SSLeay.pm
 #   inc/Test/Net/SSLeay/Socket.pm
 #   lib/Net/SSLeay/Handle.pm
-$VERSION = '1.91_01';
+$VERSION = '1.91_02';
 
 @ISA = qw(Exporter);
 
